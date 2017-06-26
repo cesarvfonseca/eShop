c1ddb45bea71fd4da5478e7adfd57c6e2f3e3fb4
cesarvfonseca <www.cavf.20@gmail.com>
2017-06-26 11:38:16 -0500

    1.0.2

--- src/java/MODELO/conexion.java
+++ src/java/MODELO/conexion.java
@@ -5,10 +5,34 @@
  */
 package MODELO;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author cvalenciano
  */
 public class conexion {
+    private String username = "root";
+    private String password="";
+    private String hostname="localhost";
+    private String port="";
+    private String database="8080";
+    private String classname="com.mysql.jdbcDriver";
+    private String url="jdbc:mysql://"+hostname+":"+port+"/"+database;
+    private Connection conn;
+    
+    public conexion(){
+        try {
+            Class.forName(classname);
+            conn = (Connection) DriverManager.getConnection(url,username,password);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public Connection getConnection (){
+        return this.conn;
+    }
     
 }
